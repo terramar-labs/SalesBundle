893b1ffa70fec4172894bc6451e6f6b0ad1fcf23
Tyler Sommer <sommertm@gmail.com>
2012-11-26 16:01:58 -0800

    Implemented office configuration CRUD

--- Controller/OfficeConfigurationController.php
+++ Controller/OfficeConfigurationController.php
@@ -19,29 +19,16 @@ use TerraMar\Bundle\SalesBundle\Form\Office\OfficeConfigurationType;
 /**
  * OfficeConfiguration controller.
  *
- * @Route("/office-configuration")
+ * @Route("/settings")
  */
 class OfficeConfigurationController extends AbstractController
 {
-    protected static $themes = array(
-        'blue',
-        'earth',
-        'green',
-        'holidays',
-        'horizon',
-        'neutral',
-        'orange',
-        'red',
-        'solitude',
-        'vegas'
-    );
-
     /**
      * Displays a form to edit an existing OfficeConfiguration entity.
      *
      * @Route("/edit", name="officeconfiguration_edit")
      * @Template()
-     * @Secure(roles="ROLE_OFFICE_WRITE")
+     * @Secure(roles="ROLE_SETTINGS_WRITE")
      */
     public function editAction(Request $request)
     {
@@ -89,9 +76,10 @@ class OfficeConfigurationController extends AbstractController
     /**
      * Edits an existing OfficeConfiguration entity.
      *
-     * @Route("/update", name="officeconfiguration_update", defaults={"_format"="json"})
+     * @Route("/update", name="officeconfiguration_update")
+     * @Template("TerraMarSalesBundle:OfficeConfiguration:edit.html.twig")
      * @Method("POST")
-     * @Secure(roles="ROLE_OFFICE_WRITE")
+     * @Secure(roles="ROLE_SETTINGS_WRITE")
      */
     public function updateAction(Request $request)
     {
@@ -116,61 +104,12 @@ class OfficeConfigurationController extends AbstractController
 
             $this->getSession()->getFlashBag()->add('success', 'Office configuration updated successfully.');
 
-            return new Response(json_encode(array('type' => 'success', 'reload' => true)));
+            return $this->redirect($this->generateUrl('officeconfiguration_edit'));
         }
-
-        return new Response(json_encode(array('type' => 'error', 'message' => 'An error occurred while updating the office configuration')));
-    }
-
-    /**
-     * Edits an existing OfficeConfiguration entity.
-     *
-     * @Route("/appearance", name="officeconfiguration_theme_edit")
-     * @Template()
-     * @Secure(roles="ROLE_OFFICE_WRITE")
-     */
-    public function editAppearanceAction(Request $request)
-    {
-        $office = $this->getCurrentOffice();
-
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('TerraMarSalesBundle:Office\OfficeConfiguration')->findOneBy(array('office' => $office->getId()));
 
         return array(
             'entity' => $entity,
-            'themes' => self::$themes
+            'form' => $form->createView(),
         );
-    }
-
-    /**
-     * Edits an existing OfficeConfiguration entity.
-     *
-     * @Route("/appearance/update", name="officeconfiguration_theme_update", defaults={"_format"="json"})
-     * @Method("POST")
-     * @Secure(roles="ROLE_OFFICE_WRITE")
-     */
-    public function updateAppearanceAction(Request $request)
-    {
-        $office = $this->getCurrentOffice();
-
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('TerraMarSalesBundle:Office\OfficeConfiguration')->findOneBy(array('office' => $office->getId()));
-
-        $theme = $request->get('theme');
-
-        if (!in_array($theme, self::$themes)) {
-            $theme = 'blue';
-        }
-
-        $entity->setTheme($theme);
-
-        $em->persist($entity);
-        $em->flush();
-
-        $this->getSession()->getFlashBag()->set('success', 'The theme has been changed. The theme will be applied when you next login.');
-
-        return new JsonReloadResponse();
     }
 }
