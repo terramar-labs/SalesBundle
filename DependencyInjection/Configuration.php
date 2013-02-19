426b508a9c0158d40bb7bb98335a867fba371c7f
Tyler Sommer <sommertm@gmail.com>
2013-02-18 23:20:56 -0800

    Changed terra_mar_sales di extension to terramar_sales

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('terra_mar_sales');
+        $rootNode = $treeBuilder->root('terramar_sales');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
