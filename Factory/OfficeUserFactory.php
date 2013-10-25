f897497c24959c5667b8752a8372e4e5140bdbd3
Tyler Sommer <sommertm@gmail.com>
2013-10-25 09:49:03 -0700

    Refactored OfficeUserFactory to be a bit more helpful

--- Factory/OfficeUserFactory.php
+++ Factory/OfficeUserFactory.php
@@ -4,6 +4,7 @@ namespace Terramar\Bundle\SalesBundle\Factory;
 
 use Doctrine\ORM\EntityManager;
 use Orkestra\Bundle\ApplicationBundle\Entity\User;
+use Orkestra\Bundle\ApplicationBundle\Model\UserInterface;
 use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
 use Terramar\Bundle\SalesBundle\Entity\Office;
 use Terramar\Bundle\SalesBundle\Entity\OfficeUser;
@@ -11,45 +12,27 @@ use Terramar\Bundle\SalesBundle\Entity\OfficeUser;
 class OfficeUserFactory implements OfficeUserFactoryInterface
 {
     /**
-     * @var \Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface
-     */
-    private $encoderFactory;
-
-    /**
      * @var \Doctrine\Common\Persistence\ObjectRepository
      */
     private $groupRepository;
 
-    public function __construct(EncoderFactoryInterface $encoderFactory, EntityManager $entityManager)
+    public function __construct(EntityManager $entityManager)
     {
-        $this->encoderFactory  = $encoderFactory;
         $this->groupRepository = $entityManager->getRepository('OrkestraApplicationBundle:Group');
     }
 
     /**
      * Creates a new user assigned to the given office
      *
-     * @param Office $office
-     * @param string $firstName
-     * @param string $lastName
-     * @param string $username
-     * @param string $password An optional password, one should be generated if none is passed
-     * @param array  $roles    An array of role names to be added as groups
+     * @param Office        $office
+     * @param UserInterface $user
+     * @param array         $roles
      *
      * @return OfficeUser
      */
-    public function createUser(Office $office, $firstName, $lastName, $username, $password = null, array $roles = array())
+    public function createOfficeUser(Office $office, UserInterface $user, array $roles = array())
     {
-        $password = $password ?: $this->generatePassword();
-
-        $user = new User();
         $officeUser = new OfficeUser($office, $user);
-
-        $encoder = $this->encoderFactory->getEncoder($user);
-        $user->setFirstName($firstName);
-        $user->setLastName($lastName);
-        $user->setUsername($username);
-        $user->setPassword($encoder->encodePassword($password, $user->getSalt()));
 
         foreach ($roles as $role) {
             $group = $this->groupRepository->findOneBy(array('role' => $role));
@@ -60,10 +43,5 @@ class OfficeUserFactory implements OfficeUserFactoryInterface
         }
 
         return $officeUser;
-    }
-
-    private function generatePassword()
-    {
-        return hash('sha256', uniqid(mt_rand(), true));
     }
 }
--- Factory/OfficeUserFactoryInterface.php
+++ Factory/OfficeUserFactoryInterface.php
@@ -2,6 +2,7 @@
 
 namespace Terramar\Bundle\SalesBundle\Factory;
 
+use Orkestra\Bundle\ApplicationBundle\Model\UserInterface;
 use Terramar\Bundle\SalesBundle\Entity\Office;
 use Terramar\Bundle\SalesBundle\Entity\OfficeUser;
 
@@ -10,14 +11,11 @@ interface OfficeUserFactoryInterface
     /**
      * Creates a new user assigned to the given office
      *
-     * @param Office $office
-     * @param string $firstName
-     * @param string $lastName
-     * @param string $username
-     * @param string $password An optional password, one should be generated if none is passed
-     * @param array  $roles    An array of role names to be added as groups
+     * @param Office        $office
+     * @param UserInterface $user
+     * @param array         $roles
      *
      * @return OfficeUser
      */
-    public function createUser(Office $office, $firstName, $lastName, $username, $password = null, array $roles = array());
+    public function createOfficeUser(Office $office, UserInterface $user, array $roles = array());
 }
