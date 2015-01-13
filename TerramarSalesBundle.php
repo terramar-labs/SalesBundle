890f95976026c0491e3dcbb8b65b29b235f8237c
Brian Wood <legion.the.unforgiven@gmail.com>
2015-01-13 16:34:05 -0700

    Added registerTypeIfNotRegistered to boot

--- TerramarSalesBundle.php
+++ TerramarSalesBundle.php
@@ -11,18 +11,15 @@ class TerramarSalesBundle extends Bundle
 {
     public function boot()
     {
-        Type::addType('enum.terramar.sales.contract_status',    'Terramar\Bundle\SalesBundle\DbalType\ContractStatusEnumType');
-        Type::addType('enum.terramar.sales.found_by_type',      'Terramar\Bundle\SalesBundle\DbalType\FoundByTypeEnumType');
+        $this->registerTypeIfNotRegistered('enum.terramar.sales.contract_status',    'Terramar\Bundle\SalesBundle\DbalType\ContractStatusEnumType');
+        $this->registerTypeIfNotRegistered('enum.terramar.sales.found_by_type',      'Terramar\Bundle\SalesBundle\DbalType\FoundByTypeEnumType');
 
-        if (!(Type::hasType('enum.terramar.sales.billing_frequency'))) {
-            Type::addType('enum.terramar.sales.billing_frequency',  'Terramar\Bundle\SalesBundle\DbalType\BillingFrequencyEnumType');
-        }
-
-        Type::addType('enum.terramar.sales.invoice_status',     'Terramar\Bundle\SalesBundle\DbalType\InvoiceStatusEnumType');
-        Type::addType('enum.terramar.sales.alert_status',       'Terramar\Bundle\SalesBundle\DbalType\AlertStatusEnumType');
-        Type::addType('enum.terramar.sales.alert_priority',     'Terramar\Bundle\SalesBundle\DbalType\AlertPriorityEnumType');
-        Type::addType('enum.terramar.sales.alert_type',         'Terramar\Bundle\SalesBundle\DbalType\AlertTypeEnumType');
-        Type::addType('enum.terramar.sales.invoice_type',       'Terramar\Bundle\SalesBundle\DbalType\InvoiceTypeEnumType');
+        $this->registerTypeIfNotRegistered('enum.terramar.sales.billing_frequency',  'Terramar\Bundle\SalesBundle\DbalType\BillingFrequencyEnumType');
+        $this->registerTypeIfNotRegistered('enum.terramar.sales.invoice_status',     'Terramar\Bundle\SalesBundle\DbalType\InvoiceStatusEnumType');
+        $this->registerTypeIfNotRegistered('enum.terramar.sales.alert_status',       'Terramar\Bundle\SalesBundle\DbalType\AlertStatusEnumType');
+        $this->registerTypeIfNotRegistered('enum.terramar.sales.alert_priority',     'Terramar\Bundle\SalesBundle\DbalType\AlertPriorityEnumType');
+        $this->registerTypeIfNotRegistered('enum.terramar.sales.alert_type',         'Terramar\Bundle\SalesBundle\DbalType\AlertTypeEnumType');
+        $this->registerTypeIfNotRegistered('enum.terramar.sales.invoice_type',       'Terramar\Bundle\SalesBundle\DbalType\InvoiceTypeEnumType');
     }
 
     /**
@@ -31,5 +28,21 @@ class TerramarSalesBundle extends Bundle
     public function build(ContainerBuilder $container)
     {
         $container->addCompilerPass(new RegisterAlertFactoriesPass());
+    }
+
+    /**
+     * Registers a type with Doctrine DBAL if it is not already registered.
+     *
+     * This is necessary because multiple instantiations of this bundle will
+     * cause an error to be thrown by the DBAL.
+     *
+     * @param string $typeName
+     * @param string $className
+     */
+    private function registerTypeIfNotRegistered($typeName, $className)
+    {
+        if (!(Type::hasType($typeName))) {
+            Type::addType($typeName, $className);
+        }
     }
 }
